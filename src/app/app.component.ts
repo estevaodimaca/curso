f79e51550696c163942de2a85686207bb83d4c6a
Estevao Pascoal Joao Dimaca <estevaodimaca@gmail.com>
2025-01-24 05:57:56 +0200

    aula 9 metade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { BaseUiComponent } from "./components/base-ui/base-ui.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, BaseUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
